cd49b9379ecef3d6c78ae7b0cf227a3130d8c77c
leebo <me@bobo.im>
2015-03-25 22:45:24 +0800

    add tmux.conf vim-trailing-whitespace and update

--- init.rb
+++ init.rb
@@ -3,8 +3,10 @@
 `rm -Rf ~/.janus`
 `rm ~/.vimrc.after`
 `rm ~/.vimrc.before`
+`rm ~/.tmux.conf`
 `ln -s ~/janus_ex ~/.janus`
 `ln -s ~/janus_ex/.vimrc.after ~/.vimrc.after`
 `ln -s ~/janus_ex/.vimrc.before ~/.vimrc.before`
+`ln -s ~/janus_ex/.tmux.conf ~/.tmux.conf`
 `git submodule init`
 `git submodule update`
